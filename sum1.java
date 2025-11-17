563e624df734426706e5c7127338c7ad7315ee44
BHADRA <bhadrabinoj2004@gmail.com>
2025-11-17 15:44:40 +0530

    Add files via upload

---
+++ sum1.java
@@ -0,0 +1,11 @@
+import java.io.*;
+class sum1 {
+	public static void main (String args[])
+	{
+	int n1,n2,sum;
+n1=Integer.parseInt(args[0]);
+n2=Integer.parseInt(args[1]);
+sum=n1+n2;
+System.out.println(sum);
+	}
+}
