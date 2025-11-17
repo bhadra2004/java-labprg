74d99b4f3f103968ae88b91cb6c2b96580f23d52
BHADRA <bhadrabinoj2004@gmail.com>
2025-11-17 15:45:17 +0530

    Add files via upload

---
+++ Variable.java
@@ -0,0 +1,13 @@
+import java.io.*;
+class Variable {
+	public static void main (String args[])
+	{
+	int a=10;	
+	int b=20;
+	int sum;
+        sum=a+b;
+	System.out.println("a="+a);
+	System.out.println("b="+b);
+	System.out.println("sum="+sum);
+	}
+}
